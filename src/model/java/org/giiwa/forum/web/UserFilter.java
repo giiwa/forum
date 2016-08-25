e1a2f305a5cd186c0807b7f87ee1face2fb8db4f
wujun <wujun@joe-mac.local>
2016-08-25 09:03:12 +0800

    init

--- src/model/java/org/giiwa/forum/web/UserFilter.java
+++ src/model/java/org/giiwa/forum/web/UserFilter.java
@@ -1,6 +1,7 @@
 package org.giiwa.forum.web;
 
 import org.giiwa.core.bean.Helper.V;
+import org.giiwa.core.task.Task;
 import org.giiwa.forum.bean.IP;
 import org.giiwa.framework.bean.User;
 import org.giiwa.framework.web.IFilter;
@@ -9,15 +10,23 @@ import org.giiwa.framework.web.Model;
 public class UserFilter implements IFilter {
 
   @Override
-  public boolean after(Model m) {
-    User u = m.getUser();
-    if (u != null && u.getString("ipfrom") == null) {
-      IP.Address a = IP.get(m.getRemoteHost());
-      if (a != null) {
-        V v = V.create("ipfrom", a.lo + " " + a.lc);
-        User.update(u.getId(), v);
+  public boolean after(final Model m) {
+    new Task() {
+
+      @Override
+      public void onExecute() {
+        User u = m.getUser();
+        if (u != null && u.getString("ipfrom") == null) {
+          IP.Address a = IP.get(m.getRemoteHost());
+          if (a != null) {
+            V v = V.create("ipfrom", a.lo + " " + a.lc);
+            User.update(u.getId(), v);
+          }
+        }
+
       }
-    }
+
+    }.schedule(0);
 
     return true;
   }
